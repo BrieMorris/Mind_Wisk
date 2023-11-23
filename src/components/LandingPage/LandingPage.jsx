400e2d11b94d63e4aabc357f7fd87e5959c7727a
Brie Morris <morris.brie@gmail.com>
2023-11-22 21:10:01 -0600

    started editing landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,32 +3,30 @@ import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
 // CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState('MindWisk');
   const history = useHistory();
 
-  const onLogin = (event) => {
-    history.push('/login');
-  };
+  // const onLogin = (event) => {
+  //   history.push('/login');
+  // };
 
   return (
     <div className="container">
-      <h2>{heading}</h2>
+      <h1>{heading}</h1>
+      <h2>Simultaneously
+      stimulating & relaxing.</h2>
+      <h2>Head massagers like the MindWisk are the      simplest, most amazing way to feel good.
+        Our first mission is to give away 1,000,000 head massagers.</h2>
+        <button> GET ONE FREE </button>
+
 
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
+            Benifits
           </p>
 
           <p>
@@ -52,14 +50,8 @@ function LandingPage() {
           </p>
         </div>
         <div className="grid-col grid-col_4">
-          <RegisterForm />
+          
 
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
         </div>
       </div>
     </div>
