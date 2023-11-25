8b0a73b0f0b081c02176cebe1c6bb7180a33e305
Getachew Hundera <ghundera1@gmail.com>
2023-11-24 21:30:04 -0600

    npm installed stripe, added test api requests in donationpage.jsx file

--- src/components/DonationPage/DonationPage.jsx
+++ src/components/DonationPage/DonationPage.jsx
@@ -5,6 +5,26 @@ import { useSelector } from 'react-redux';
 function DonationPage() {
   const user = useSelector((store) => store.user);
 
+
+  const stripe = require('stripe')('sk_test_');
+
+stripe.products.create({
+  name: 'Starter Subscription',
+  description: '$12/Month subscription',
+}).then(product => {
+  stripe.prices.create({
+    unit_amount: 1200,
+    currency: 'usd',
+    recurring: {
+      interval: 'month',
+    },
+    product: product.id,
+  }).then(price => {
+    console.log('Success! Here is your starter subscription product id: ' + product.id);
+    console.log('Success! Here is your starter subscription price id: ' + price.id);
+  });
+});
+
   return (
     <div className="donationpage">
 
