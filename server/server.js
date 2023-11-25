070c4f5d7c7cfc0849758669273b0d3afeba230c
Getachew Hundera <ghundera1@gmail.com>
2023-11-24 21:43:47 -0600

    created stripe router in routes, imported stripe

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+import cors from 'cors';
 const app = express();
 
 const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/user.strategy');
 
+
 // Route includes
 const userRouter = require('./routes/user.router');
+const stripeRouter = require('./routes/stripe.router'); 
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -20,9 +22,13 @@ app.use(sessionMiddleware);
 // start up passport sessions
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors()); 
+app.use(json()); 
+
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/stripe', stripeRouter); 
 
 // Serve static files
 app.use(express.static('build'));
