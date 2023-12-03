701fc49f289bc943f097d3b61d6e06aa19e54db2
Getachew Hundera <ghundera1@gmail.com>
2023-12-03 02:53:35 -0600

    added a useEffect to create paymentintent on load, added use state for the clientSecret

--- src/components/DonationPage/DonationPage.jsx
+++ src/components/DonationPage/DonationPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as React from 'react';
-import './DonationPage.css'; 
+import './DonationPage.css';
 
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -21,7 +21,7 @@ import PaymentForm from './PaymentForm';
 import Review from './Review';
 import DonationForm from './DonationForm';
 
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from "react";
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
@@ -48,7 +48,7 @@ function Copyright() {
       {'.'}
     </Typography>
   );
-}
+};
 
 const steps = ['Donate Selection', 'Shipping address', 'Payment details', 'Review your order'];
 
@@ -65,19 +65,35 @@ function getStepContent(step) {
     default:
       throw new Error('Unknown step');
   }
-}
+};
 
 //Calling `loadStripe` outside of a component's to render to avoid recreating the `Stripe` obect on every render. 
 // intizalize Stripe Object 
 // calling loadStripe loads latest version of stripe.js, helps with security. 
 const stripePromise = loadStripe('pk_test_fillin');  // fill in the the test 
 
+function DonationPage() {
 
-export default function DonationPage() {
+  const [clientSecret, setClientSecret] = useState("");
 
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
   const options = {
     // passing the client secret obtained from the server
     clientSecret: '{{CLIENT_SECRET}}',
+    appearance,
   };
 
 
@@ -92,71 +108,76 @@ export default function DonationPage() {
   };
 
   return (
-    <Elements stripe={stripePromise} options={options} > 
-    <React.Fragment>
-      <CssBaseline />
-      <AppBar
-        position="absolute"
-        color="default"
-        elevation={0}
-        sx={{
-          position: 'relative',
-          borderBottom: (t) => `1px solid ${t.palette.divider}`,
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            MindWisk
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-          <Typography component="h1" variant="h4" align="center">
-            Checkout
-          </Typography>
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          {activeStep === steps.length ? (
-            <React.Fragment>
-              <Typography variant="h5" gutterBottom>
-                Thank you for your order.
-              </Typography>
-              <Typography variant="subtitle1">
-                Your order number is #2001539. We have emailed your order
-                confirmation, and will send you an update when your order has
-                shipped.
-              </Typography>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              {getStepContent(activeStep)}
-              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                {activeStep !== 0 && (
-                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                    Back
-                  </Button>
-                )}
+    <div className='donationPage'>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <DonationCheckoutForm />
+          <React.Fragment>
+            <CssBaseline />
+            <AppBar
+              position="absolute"
+              color="default"
+              elevation={0}
+              sx={{
+                position: 'relative',
+                borderBottom: (t) => `1px solid ${t.palette.divider}`,
+              }}
+            >
+              <Toolbar>
+                <Typography variant="h6" color="inherit" noWrap>
+                  MindWisk
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+              <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                <Typography component="h1" variant="h4" align="center">
+                  Checkout
+                </Typography>
+                <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+                  {steps.map((label) => (
+                    <Step key={label}>
+                      <StepLabel>{label}</StepLabel>
+                    </Step>
+                  ))}
+                </Stepper>
+                {activeStep === steps.length ? (
+                  <React.Fragment>
+                    <Typography variant="h5" gutterBottom>
+                      Thank you for your order.
+                    </Typography>
+                    <Typography variant="subtitle1">
+                      Your order number is #2001539. We have emailed your order
+                      confirmation, and will send you an update when your order has
+                      shipped.
+                    </Typography>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    {getStepContent(activeStep)}
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                      {activeStep !== 0 && (
+                        <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                          Back
+                        </Button>
+                      )}
 
-                <Button
-                  variant="contained"
-                  onClick={handleNext}
-                  sx={{ mt: 3, ml: 1 }}
-                >
-                  {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                </Button>
-              </Box>
-            </React.Fragment>
-          )}
-        </Paper>
-        <Copyright />
-      </Container>
-    </React.Fragment>
-</Elements>
+                      <Button
+                        variant="contained"
+                        onClick={handleNext}
+                        sx={{ mt: 3, ml: 1 }}
+                      >
+                        {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                      </Button>
+                    </Box>
+                  </React.Fragment>
+                )}
+              </Paper>
+              <Copyright />
+            </Container>
+          </React.Fragment>
+        </Elements>
+      )}
+    </div>
   );
-}; 
+};
