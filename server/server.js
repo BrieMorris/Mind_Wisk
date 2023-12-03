1e3536939d96fc39bbc2d53e226d93a39c10e541
Getachew Hundera <ghundera1@gmail.com>
2023-12-03 00:13:29 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
+// const stripe = require ("stripe")('sk_test_fill in test security key'); 
+
 
 const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/user.strategy');
@@ -11,6 +13,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const stripeRouter = require('./routes/stripe.router'); 
+const stripePaymentIntentRouter = require('./routes/stripe.paymentIntent.router'); 
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -30,7 +33,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/stripe', stripeRouter); 
+app.use('/api/PaymentMethod', stripePaymentMethodRouter);
+app.use('/api/PaymentIntent, stripePaymentIntentRouter') 
 
 // Serve static files
 app.use(express.static('build'));
