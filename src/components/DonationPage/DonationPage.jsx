fd18f905ff8d69d9c6a807a3d6e518238017090c
Getachew Hundera <ghundera1@gmail.com>
2023-12-05 23:20:53 -0600

    commit

--- src/components/DonationPage/DonationPage.jsx
+++ src/components/DonationPage/DonationPage.jsx
@@ -17,7 +17,7 @@ import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import AddressForm from './AddressForm';
-import PaymentForm from './PaymentForm';
+// import PaymentForm from './PaymentForm';
 import Review from './Review';
 import DonationForm from './DonationForm';
 
@@ -25,9 +25,8 @@ import DonationForm from './DonationForm';
 
 
 
-import DonationCheckoutForm from './DonationCheckoutForm';
+import DonationCheckoutForm from "./DonationCheckoutForm";
 // Payment Intent and Parent component for paymentelement 
-
 
 
 
@@ -55,7 +54,7 @@ function getStepContent(step) {
     case 1:
       return <AddressForm />;
     case 2:
-      return <PaymentForm />;
+      return <DonationCheckoutForm />;
     case 3:
       return <Review />;
     default:
@@ -67,6 +66,11 @@ function getStepContent(step) {
 // intizalize Stripe Object 
 // calling loadStripe loads latest version of stripe.js, helps with security. 
 const stripePromise = loadStripe('pk_test_fillin');  // fill in the the test 
+
+
+
+
+
 
 function DonationPage() {
 
@@ -106,76 +110,74 @@ function DonationPage() {
   return (
     <div className='donationPage'>
       {clientSecret && (
-        <React.Fragment>
-          <Elements options={options} stripe={stripePromise}>
-            <DonationCheckoutForm />
-          </Elements>
-          <CssBaseline />
-          <AppBar
-            position="absolute"
-            color="default"
-            elevation={0}
-            sx={{
-              position: 'relative',
-              borderBottom: (t) => `1px solid ${t.palette.divider}`,
-            }}
-          >
-            <Toolbar>
-              <Typography variant="h6" color="inherit" noWrap>
-                MindWisk
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-            <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-              <Typography component="h1" variant="h4" align="center">
-                Checkout
-              </Typography>
-              <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-                {steps.map((label) => (
-                  <Step key={label}>
-                    <StepLabel>{label}</StepLabel>
-                  </Step>
-                ))}
-              </Stepper>
-              {activeStep === steps.length ? (
-                <React.Fragment>
-                  <Typography variant="h5" gutterBottom>
-                    Thank you for your order.
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    Your order number is #2001539. We have emailed your order
-                    confirmation, and will send you an update when your order has
-                    shipped.
-                  </Typography>
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-                  {getStepContent(activeStep)}
-                  <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                    {activeStep !== 0 && (
-                      <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                        Back
-                      </Button>
-                    )}
+        <Elements options={options} stripe={stripePromise}>
+          <React.Fragment>
+            <CssBaseline />
+            <AppBar
+              position="absolute"
+              color="default"
+              elevation={0}
+              sx={{
+                position: 'relative',
+                borderBottom: (t) => `1px solid ${t.palette.divider}`,
+              }}
+            >
+              <Toolbar>
+                <Typography variant="h6" color="inherit" noWrap>
+                  MindWisk
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+              <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                <Typography component="h1" variant="h4" align="center">
+                  Checkout
+                </Typography>
+                <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+                  {steps.map((label) => (
+                    <Step key={label}>
+                      <StepLabel>{label}</StepLabel>
+                    </Step>
+                  ))}
+                </Stepper>
+                {activeStep === steps.length ? (
+                  <React.Fragment>
+                    <Typography variant="h5" gutterBottom>
+                      Thank you for your Donation.
+                    </Typography>
+                    <Typography variant="subtitle1">
+                      Your Donation Confirmatin number is is #2001539. We have emailed your recipt
+                      confirmation.
+                    </Typography>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    {getStepContent(activeStep)}
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                      {activeStep !== 0 && (
+                        <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                          Back
+                        </Button>
+                      )}
 
-                    <Button
-                      variant="contained"
-                      onClick={handleNext}
-                      sx={{ mt: 3, ml: 1 }}
-                    >
-                      {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                    </Button>
-                  </Box>
-                </React.Fragment>
-              )}
-            </Paper>
-            <Copyright />
-          </Container>
-        </React.Fragment>
-      )};
-    </div>
-  );
+                      <Button
+                        variant="contained"
+                        onClick={handleNext}
+                        sx={{ mt: 3, ml: 1 }}
+                      >
+                        {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                      </Button>
+                    </Box>
+                  </React.Fragment>
+                )}
+              </Paper>
+              <Copyright />
+            </Container>
+          </React.Fragment>
+      )}
+      
+  };
+        </div>
 };
 
-export default DonationPage; 
+      export default DonationPage; 
