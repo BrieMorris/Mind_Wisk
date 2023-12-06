94b6585bb66f6a81c31710caa830dfef755062f0
Getachew Hundera <ghundera1@gmail.com>
2023-12-06 12:59:11 -0600

    updated syntax for payment intent post endpoint in stripePaymentIntent.router.js file, updated the eroor messaging

--- server/routes/stripePaymentIntent.router.js
+++ server/routes/stripePaymentIntent.router.js
@@ -1,40 +1,39 @@
 const express = require('express');
-const Stripe = require('stripe'); 
-const router = express.Router(); 
+const Stripe = require('stripe');
+const router = express.Router();
 
-const stripe = new Stripe('sk_test_fill in test security key'); 
+const stripe = new Stripe('sk_test_fill in test security key');
 
 
-export default function createStripeRoutes(stripe) {
-    const router = express.Router();
-/**
- * POST route template
- */
-    router.post('/api/create-payment-intent', async (req, res) => {
-        const { amount } = req.body;
-        try {
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount: amount,
-                currency: 'usd',
-                automatic_payment_methods: { enabled: true },
-            });
-            res.send({
-                clientSecret: paymentIntent.client_secret, 
-              });               
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: error.message });
-        }
-    })
-    return router;
-}
 
 /**
  * GET route template
  */
 router.get('/create-payment-intent', (req, res) => {
-  // GET route code here
-res.send('Hello World!')
+    // GET route code here
+    res.send('Hello World!');
 });
+
+/**
+ * POST route template
+ */
+router.post('/', async (req, res) => {
+    const { amount } = req.body;
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount, //check that it is a number and not a string 
+            currency: 'usd',
+            automatic_payment_methods: { enabled: true },
+        });
+        res.send({
+            clientSecret: paymentIntent.client_secret,
+        });
+    } catch (error) {
+        console.log("Error creating payment intent:", error);
+        res.status(500).send({ error: error.message });
+    }
+});
+
+
 
 module.exports = router;
