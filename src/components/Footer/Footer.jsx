b8384afeba6f307e9cc1adf0b6830d40434c6317
Rafael Silva <rafsilva004@gmail.com>
2023-12-11 20:10:17 -0600

    final changes to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,64 +23,67 @@ function Footer() {
 
   function resizeImg(img, newWidth) {
     img.width = newWidth;
-  }  
+  }
 
   return (
     <footer>
-   <Grid container justifyContent="space-between">
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        {/* Leftmost grid content */}
-        <img src={logo} alt="MindWisk Logo" onLoad={(event) => resizeImg(event.target, 150)} />
-        <br /><br />
-        <Typography variant="body2" style={{ textAlign: 'left', fontSize: '1.5rem' }}>
-          &copy; MindWisk
-        </Typography>
-        <br />
-        <Button variant="outlined" onClick={disclaimer} style={{ color: 'white', backgroundColor: '#50C878' }}>
-          Disclaimer
-        </Button>
-      </Grid>
-
-      <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        {/* Center grid content */}
-        <div className="center-content">
-      </div>
-        <br /><br />
-        <Typography variant="h6">CONTACT US:</Typography>
-          <br />
-          <Typography variant="body2">MindWisk@gmail.com</Typography>
-          <br />
-      </Grid>
-
-      <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'right', justifyContent: 'right' }}> 
-        {/* Rightmost grid content */}
-        <div className="right-content">
+      <Grid container justifyContent="space-between">
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          {/* Leftmost grid content */}
+          <img src={logo} alt="MindWisk Logo" onLoad={(event) => resizeImg(event.target, 150)} />
           <br /><br />
-          <Typography variant="h6">FOLLOW US</Typography>
+          <Typography variant="body2" style={{ textAlign: 'left', fontSize: '1.5rem' }}>
+            &copy; MindWisk
+          </Typography>
           <br />
-
-          <a href="https://www.facebook.com/MindWisk" target="_blank" rel="noopener noreferrer">
-            <FacebookIcon />
-          </a>
-
-          <a href="https://www.instagram.com/mindwiskproject" target="_blank" rel="noopener noreferrer">
-            <InstagramIcon />
-          </a>
-
-          <a href="https://twitter.com/MindWiskProject" target="_blank" rel="noopener noreferrer">
-            <ClearIcon />
-          </a>
-
-          <a href="https://www.youtube.com/@MindWisk" target="_blank" rel="noopener noreferrer">
-            <YouTubeIcon />
-          </a>
-          <br /><br /><br /><br />
-          <Button variant="outlined" onClick={admin} style={{ color: 'white', backgroundColor: '#50C878' }}>
-            Admin
+          <Button variant="outlined" onClick={disclaimer} style={{ color: 'white', backgroundColor: '#50C878' }}>
+            Disclaimer
           </Button>
-        </div>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          {/* Center grid content */}
+          <div className="center-content">
+          </div>
+          <br /><br />
+          <Typography variant="h6">CONTACT US:</Typography>
+          <br />
+          <a href="mailto:MindWisk@gmail.com" style={{ color: 'white' }}>
+            <Typography variant="body2">MindWisk@gmail.com</Typography>
+          </a>
+          <br />
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+          {/* Rightmost grid content */}
+          <div className="right-content" style={{ marginBottom: '40px' }}>
+            <br /><br />
+            <Typography variant="h6">FOLLOW US</Typography>
+            <br />
+
+            <a href="https://www.facebook.com/MindWisk" target="_blank" rel="noopener noreferrer">
+              <FacebookIcon />
+            </a>
+
+            <a href="https://www.instagram.com/mindwiskproject" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon />
+            </a>
+
+            <a href="https://twitter.com/MindWiskProject" target="_blank" rel="noopener noreferrer">
+              <ClearIcon />
+            </a>
+
+            <a href="https://www.youtube.com/@MindWisk" target="_blank" rel="noopener noreferrer">
+              <YouTubeIcon />
+            </a>
+            <br /><br /><br /><br />
+            <Button variant="outlined" onClick={admin} style={{ color: 'white', backgroundColor: '#50C878' }}>
+              Admin
+            </Button>
+          </div>
+        </Grid>
+
       </Grid>
-    </Grid>
     </footer>
   );
 }
